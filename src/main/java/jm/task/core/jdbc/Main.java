6aa461f29529fb5663cb0c2983dd7c7dfbc805bc
buenelefante <nwpiter@hotmail.com>
2022-04-16 17:05:18 +0300

    setup connection to db

--- src/main/java/jm/task/core/jdbc/Main.java
+++ src/main/java/jm/task/core/jdbc/Main.java
@@ -1,7 +1,27 @@
 package jm.task.core.jdbc;
 
+import jm.task.core.jdbc.util.Util;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Main {
     public static void main(String[] args) {
         // реализуйте алгоритм здесь
+        Util util = new Util();
+        String query = "select * from users";
+
+        try {
+            Statement statement = util.getConnection().createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+            while (resultSet.next()) {
+                String name = resultSet.getString(2);
+                System.out.println(name);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,29 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    private static final String URL = "jdbc:mysql://localhost:3306/javatest";
+    private static  final String USERNAME = "root";
+    private  static final String PASSWORD = "mySQL2022";
+
+    private Connection connection;
+
+    public Connection getConnection(){
+        return connection;
+    }
+    public void setConnection(Connection connection) {
+        this.connection = connection;
+    }
+    public Util() {
+        try {
+            connection = DriverManager.getConnection(URL,USERNAME, PASSWORD);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
 }
